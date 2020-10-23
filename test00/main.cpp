a64b37bd27f6698b2e0ef8de8f77a3181f14921d
pj <xiaopcxy@163.com>
2020-10-23 20:54:27 +0800

    1.windows socket 基础

--- test00/main.cpp
+++ test00/main.cpp
@@ -1,12 +1,18 @@
+#define WIN32_LEAN_AND_MEAN
 #include <iostream>
+#include <Windows.h>
+#include <WinSock2.h>
 using namespace std;
 
+// #pragma comment(lib, "ws2_32.lib")
 int main()
 {
-	cout << "hello c++" << endl;
-	int a = 0;
-	for (int i = 1; i <= 10; ++i)
-	{
-		a = a + i;
-	}
+	// Æô¶¯socket
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+	
+	// ÇåÀísocket
+	WSACleanup();
+	return 0;
 }
